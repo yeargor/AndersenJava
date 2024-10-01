285cffcb1a3d0015491faec355ba54a6d8e29285
yeargor <168727334+yeargor@users.noreply.github.com>
2024-10-01 18:26:24 +0300

    Homework#8 (#18)
    * Homework#8
    
    * pom file added
    
    * fixes
    
    * fixes

---
+++ src/main/java/Homework8/config/JDBCConfig.java
@@ -0,0 +1,24 @@
+package Homework8.config;
+
+import Homework8.model.Ticket;
+import Homework8.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class JDBCConfig {
+
+    private static final String HIBERNATE_CFG_XML = "hibernate.cfg.xml";
+
+    public static SessionFactory getSessionFactory(){
+        return new Configuration()
+                .configure(HIBERNATE_CFG_XML)
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Ticket.class)
+                .buildSessionFactory();
+    }
+
+    public static Session getSession(){
+        return getSessionFactory().getCurrentSession();
+    }
+}
